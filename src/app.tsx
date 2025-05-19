a2a58e62c58b05eeea7b3a78b57fba7e1f8039fa
Wojtek Malarski <w.malarski@gmail.com>
2025-05-19 22:45:27 +0200

    adds i18n context and head

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,21 @@
-import { MetaProvider, Title } from "@solidjs/meta";
+import { MetaProvider } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import "./app.css";
+import { Head } from "./modules/shared/head";
+import { I18nContextProvider } from "./modules/shared/i18n";
 
 export default function App() {
   return (
     <Router
       root={(props) => (
-        <MetaProvider>
-          <Title>SolidStart - Basic</Title>
-          <a href="/">Index</a>
-          <a href="/about">About</a>
-          <Suspense>{props.children}</Suspense>
-        </MetaProvider>
+        <I18nContextProvider>
+          <MetaProvider>
+            <Head />
+            <Suspense>{props.children}</Suspense>
+          </MetaProvider>
+        </I18nContextProvider>
       )}
     >
       <FileRoutes />
