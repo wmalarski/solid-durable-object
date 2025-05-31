3a39922d28ab6e9de3e05c54e5d9e60f13109dad
Wojtek Malarski <w.malarski@gmail.com>
2025-05-31 09:30:27 +0200

    got hono working locally

--- src/api/route.ts
+++ src/api/route.ts
@@ -1,11 +1,17 @@
 import { Hono } from "hono";
+import { $DurableObject } from "../modules/board/durable/durable";
 
-const app = new Hono().basePath("/api");
+const app = new Hono<{ Bindings: Env }>();
 
 app.get("/hello", (c) => {
+  // console.log("c.env", c.env, c.event);
   return c.json({
     message: "Hello Next.js!",
   });
 });
+
+app.get("*", async (c) => c.env.ASSETS.fetch(c.req.raw));
+
+export { $DurableObject };
 
 export default app;
--- vite.config.ts
+++ vite.config.ts
@@ -25,9 +25,66 @@ export default defineConfig({
       entry: "./src/api/route.ts",
       exclude: [/^(?!\/api)\/.*/],
     }),
-    // include: [/^\/api/],
   ],
+  server: {
+    proxy: {
+      "/api": {
+        rewrite: (path: string) => path.replace(/^\/api/, "/api"),
+      },
+    },
+  },
 });
+
+/*
+
+export default defineConfig(async ({ mode }) => {
+  if (mode === 'client') {
+    return {
+      plugins: [
+        viteReact(),
+        TanStackRouterVite({ routesDirectory: 'app/routes', generatedRouteTree: "app/routeTree.gen.ts" })],
+      resolve,
+      server: {
+        proxy: {
+          '/api': {
+            target: 'http://localhost:3000',
+            changeOrigin: true,
+            rewrite: (path: string) => path.replace(/^\/api/, '/api'),
+          }
+        },
+      },
+    }
+  } else {
+    const isDev = process.env.NODE_ENV === 'development';
+    let devServerPlugin;
+    if (isDev) {
+      const { env, dispose } = await getPlatformProxy();
+      devServerPlugin = devServer({
+        entry: 'app/api/index.tsx',
+        adapter: {
+          env,
+          onServerClose: dispose
+        },
+      });
+    }
+
+    return {
+      plugins: [
+        pages({
+          entry: ['app/api/index.tsx'],
+        }),
+        devServerPlugin,
+      ],
+      resolve,
+      server: {
+        port: 3000,
+      },
+    }
+  }
+})
+
+*/
+
 /*
 export default defineConfig({
   server: {
